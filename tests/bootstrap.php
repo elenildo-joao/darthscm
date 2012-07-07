498f85022d134a0df2ad72964910503767b407c5
Silas Alves <silinhasalves@gmail.com>
2012-07-07 11:14:35 -0300

    Primeiro Commit Silas

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 
+error_reporting(E_ALL | E_STRICT);
+
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../../application'));
 
 // Define application environment
 defined('APPLICATION_ENV')
@@ -14,5 +16,14 @@ set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path(),
 )));
 
-require_once 'Zend/Loader/Autoloader.php';
-Zend_Loader_Autoloader::getInstance();
+
+require_once 'Zend/Application.php';
+//require_once 'Zend/Loader/Autoloader.php';
+
+require_once 'ControllerTestCase.php';
+
+
+//Zend_Loader_Autoloader::getInstance();
+
+//require_once APPLICATION_PATH . '/../library/Zend/Test/PHPUnit/ControllerTestCase.php';
+?> 
