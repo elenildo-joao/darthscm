d69a7e240dabdeefe072f0a5aeb334f3587b8382
jacquelinemidlej <jacquelinemidlej@gmail.com>
2012-07-13 03:05:11 -0300

    Correção da função tratainterval

--- application/models/Projetos.php
+++ application/models/Projetos.php
@@ -14,39 +14,53 @@ class Projetos extends Zend_Db_Table_Abstract
             'refColumns'    => 'idrepositorio'
         )
     ); 
-            public function trataInterval ($tempo, $int1, $int2, $d2, $h2, $m2){ 
-            $x=0;  
-            $flagd=0; 
+    public function trataInterval ($tempo, $int1, $int2, $d2, $h2, $m2){ 
+    $x=0;  
+    $flagd=0;
 
-            while ($x<strlen($tempo)-1){
-                if ($tempo[$x]=='d'){
-                   for ($y=0; $y<$x-1; $y++)
-                       $d2=$d2.$tempo[$y];
-                   $flagd=1;
-                   break;
-                }
-                $x++;
-            }
-            if ($flagd==0)
-                $x=0;
-            while ($x<strlen($tempo)-1){
-                if ($tempo[$x]==' '){ $x++;
-                    while ($tempo[$x]!=':')
-                    $h2=$h2.$tempo[$x++];
-                   // if ($x==':'){
-                    $x++;
-                    while ($tempo[$x]!=':')
-                        $m2=$m2.$tempo[$x++];
-                        //if ($x==':')
-                
-                }
+    while ($x<strlen($tempo)-1){
+        if ($tempo[$x]=='d'){
+           for ($y=0; $y<$x-1; $y++)
+               $d2=$d2.$tempo[$y];
+           $flagd=1;
+           break;
+        }
+        $x++;
+    }
+    
+    if ($flagd==0)
+        $x=0;
+    
+    $flagd=0;
+    
+    while ($x<strlen($tempo)-1){
+        if ($tempo[$x]==' '){
             $x++;
-            }
-            $int2->d=$d2;
-            $int2->h=$h2;
-            $int2->i=$m2; 
-            } 
-     public function SomaInterval($int1, $int2){
+            $flagd=1;
+            while ($tempo[$x]!=':')
+                $h2=$h2.$tempo[$x++];
+            $x++;
+            while ($tempo[$x]!=':')
+                $m2=$m2.$tempo[$x++];
+        }
+        $x++;
+    }
+    if ($flagd==0){
+        $x=0;
+        while ($x<strlen($tempo)-1){
+            while ($tempo[$x]!=':')
+                $h2=$h2.$tempo[$x++];
+            $x++;
+            while ($tempo[$x]!=':')
+                $m2=$m2.$tempo[$x++];
+            $x++;
+        }            
+    }
+    $int2->d=$d2;
+    $int2->h=$h2;
+    $int2->i=$m2; 
+    } 
+    public function SomaInterval($int1, $int2){
         $min=$int2->i+$int1->i;
         $hora=$int2->h+$int1->h;
         $dia=$int2->d+$int1->d;
@@ -62,6 +76,6 @@ class Projetos extends Zend_Db_Table_Abstract
         $int2->h=$hora;
         $int2->d=$dia;
         return $int2;
-     }
+    }
 }
 
