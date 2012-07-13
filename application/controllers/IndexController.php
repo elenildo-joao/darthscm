205deb87851188b8f8d91c486bbed3d679b31013
jacquelinemidlej <jacquelinemidlej@gmail.com>
2012-07-12 22:58:22 -0300

    correção de erro em index

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,6 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-         echo $this->url(array('controller' => 'login', 'action' => 'index')); 
     }
 
 
