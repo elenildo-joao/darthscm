fc215cc1c500ac43bed1ef0cc150ce34aa872759
jacquelinemidlej <jacquelinemidlej@gmail.com>
2012-07-17 03:03:58 -0300

    Módulo de mensagens - cont.

--- application/controllers/MensagensController.php
+++ application/controllers/MensagensController.php
@@ -119,8 +119,8 @@ class MensagensController extends Zend_Controller_Action
 
         $dadosRemove = array('excluida' => 't', 'lixeira' => 'f' );
 
-        $whereRemove = $this->destinatario->getAdapter()
-               ->quoteInto('idmensagem = ?', (int) $idMensagem);
+        $whereRemove = $this->destinatario->getAdapter()->quoteInto('idmensagem = ?', (int) $idMensagem).
+                       $this->destinatario->getAdapter()->quoteInto('AND destinatario = ?', $this->usuarioLogado->idusuario);
            
         $this->destinatario->update($dadosRemove, $whereRemove);
         $this->view->mensagemErro='Mensagem Removida com Sucesso!';
@@ -132,9 +132,8 @@ class MensagensController extends Zend_Controller_Action
 
         $dadosRemove = array('excluida' => 't', 'lixeira' => 'f');
 
-        $whereRemove = $this->mensagem->getAdapter()
-               ->quoteInto('idmensagem = ?', (int) $idMensagem);
-        
+        $whereRemove = $this->mensagem->getAdapter()->quoteInto('idmensagem = ?', (int) $idMensagem).
+                       $this->mensagem->getAdapter()->quoteInto('AND remetente = ?', (int) $this->usuarioLogado->idusuario);
         $this->mensagem->update($dadosRemove, $whereRemove);
         $this->view->mensagemErro='Mensagem Removida com Sucesso!';
 //        $this->_redirect('/usuarios/listar');
@@ -154,12 +153,14 @@ class MensagensController extends Zend_Controller_Action
                  ->fetchAll(
                      $this->vMsgEnviada->select()->where('idmensagem = ?', $idMensagem)
                   );
+        $a=(boolean)false;
         $dadosVisualiza = array('msglida' => 't');
 
-        $whereVisualiza = $this->destinatario->getAdapter()
-               ->quoteInto(array('idmensagem = ?' => (int) $idMensagem, 'destinatario' => $this->usuarioLogado->idusuario));
-           
-        $this->destinatario->update($dadosVisualiza, $whereVisualiza);
+//        $whereVisualiza = $this->destinatario->getAdapter()->quoteInto('idmensagem = '.$idMensagem.' AND destinatario='.$this->usuarioLogado->idusuario, 0);
+        $whereVisualiza = $this->destinatario->getAdapter()->quoteInto('idmensagem = ?', $idMensagem).
+                          $this->destinatario->getAdapter()->quoteInto('AND destinatario = ? ', $this->usuarioLogado->idusuario);
+            $this->destinatario->update($dadosVisualiza, $whereVisualiza);   
+
     }
     
     public function visualizarSaidaAction() {
